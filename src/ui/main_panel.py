d0494ea309e6922396debdb6d3129e504d0b02eb
bgoldbeck <bgoldbeck21@gmail.com>
2019-01-15 22:48:09 -0800

    Adding the threading system files

--- src/ui/main_panel.py
+++ src/ui/main_panel.py
@@ -9,6 +9,7 @@
 # This software is licensed under the MIT License. See LICENSE file for the full text.
 import wx
 from src.ui.opengl_canvas import OpenGLCanvas
+from src.threading.thread_manager import *
 
 
 class MainPanel(wx.Panel):
@@ -27,3 +28,6 @@ class MainPanel(wx.Panel):
         self.canvas = OpenGLCanvas(self)
 
         self.parent.Layout()
+
+        wx.PaintDC(self)
+        self.Refresh()
